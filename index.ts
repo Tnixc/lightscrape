f928d521a77d519c3268ce9e0df725b82951bd62
Tnixc <tnixxc@gmail.com>
2024-02-22 23:42:12 -0500

    Refactor code to improve performance and readability

--- index.ts
+++ index.ts
@@ -1,24 +1,10 @@
-//@ts-expect-error
-import * as http from 'http';
+import { downloadHtml } from "./utils";
 
-function downloadHtml(url: string): Promise<string> {
-  return new Promise((resolve, reject) => {
-    http.get(url, (res) => {
-      let data = '';
-      res.on('data', (chunk) => {
-        data += chunk;
-      });
-      res.on('end', () => {
-        resolve(data);
-      });
-    }).on('error', (err) => {
-      reject(err);
-    });
-  });
+if (process.argv.length === 2) {
+  console.error('Expected at least one argument!');
+  process.exit(1);
 }
 
-// Usage example
-const url = 'https://webnovelpub.co/web-novel-pub/building-a-business-empire-with-my-technological-system';
 downloadHtml(url)
   .then((html) => {
     console.log(html);
@@ -26,3 +12,8 @@ downloadHtml(url)
   .catch((err) => {
     console.error('Error:', err);
   });
+
+
+console.log("Url: ", process.argv.slice(2, 3).toString());
+const url = process.argv.slice(2, 3).toString();
+
---
+++ utils.ts
@@ -0,0 +1,20 @@
+//@ts-expect-error
+import * as http from 'http';
+
+export async function downloadHtml(url: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    http.get(url, (res) => {
+      let data = '';
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+      res.on('end', () => {
+        resolve(data);
+      });
+    }).on('error', (err) => {
+      reject(err);
+    });
+  });
+}
+
+console.log(await downloadHtml("https://example.com"))
